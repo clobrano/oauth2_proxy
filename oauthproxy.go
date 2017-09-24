080c5b4c47c95d256d7aac8355d442f9d7ee13da
Carlo Lobrano <c.lobrano@gmail.com>
2017-09-24 23:38:04 +0200

    Added call to GetUserName in redeemCode
    This commit uses new GetUserName method in redeemCode
    to get SessionState User value.
    
    For backward compatibility, if GetUserName error is
    "not implemented", the error is ignored.

--- oauthproxy.go
+++ oauthproxy.go
@@ -248,6 +248,13 @@ func (p *OAuthProxy) redeemCode(host, code string) (s *providers.SessionState, e
 	if s.Email == "" {
 		s.Email, err = p.provider.GetEmailAddress(s)
 	}
+
+	if s.User == "" {
+		s.User, err = p.provider.GetUserName(s)
+		if err.Error() == "not implemented" {
+			err = nil
+		}
+	}
 	return
 }
 
