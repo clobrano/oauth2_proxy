830440ef82b5170fd548939a4667d5a5985864fc
Karl Fischer <karl.fischer@project-a.com>
2017-11-07 17:21:29 +0100

    Keep 'if' indentation level small

--- oauthproxy.go
+++ oauthproxy.go
@@ -247,10 +247,8 @@ func (p *OAuthProxy) redeemCode(host, code string) (s *providers.SessionState, e
 
 	if s.User == "" {
 		s.User, err = p.provider.GetUserName(s)
-		if err != nil {
-			if err.Error() == "not implemented" {
-				err = nil
-			}
+		if err != nil && err.Error() == "not implemented" {
+			err = nil
 		}
 	}
 	return
